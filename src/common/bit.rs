1e1e2c3b6d6d6ebb6534e6005244725f05f7b22a
Firman Hadi P <painneon@gmail.com>
2024-10-31 03:12:21 +0800

    Fix warnings

--- src/common/bit.rs
+++ src/common/bit.rs
@@ -1,45 +1,45 @@
 // Bit manipulation library
 
-pub const fn bit<const Pos: u8>() -> u32 {
-  bit_range::<Pos, Pos>()
+pub const fn bit<const POS: u8>() -> u32 {
+  bit_range::<POS, POS>()
 }
 
-pub const fn bit_range<const Msb: u8, const Lsb: u8>() -> u32 {
-  if Msb < Lsb {
-    return bit_range::<Lsb, Msb>();
+pub const fn bit_range<const MSB: u8, const LSB: u8>() -> u32 {
+  if MSB < LSB {
+    return bit_range::<LSB, MSB>();
   }
-  ((1 << Msb) | ((1 << Msb) - 1)) & !((1 << Lsb) - 1)
+  ((1 << MSB) | ((1 << MSB) - 1)) & !((1 << LSB) - 1)
 }
 
-pub const fn bit_u64<const Pos: u8>() -> u64 {
-  bit_range_u64::<Pos, Pos>()
+pub const fn bit_u64<const POS: u8>() -> u64 {
+  bit_range_u64::<POS, POS>()
 }
 
-pub const fn bit_range_u64<const Msb: u8, const Lsb: u8>() -> u64 {
-  if Msb < Lsb {
-    return bit_range_u64::<Lsb, Msb>();
+pub const fn bit_range_u64<const MSB: u8, const LSB: u8>() -> u64 {
+  if MSB < LSB {
+    return bit_range_u64::<LSB, MSB>();
   }
-  ((1 << Msb) | ((1 << Msb) - 1)) & !((1 << Lsb) - 1)
+  ((1 << MSB) | ((1 << MSB) - 1)) & !((1 << LSB) - 1)
 }
 
-pub fn bit_of<const Pos: u8>(val: u32) -> u32 {
-  (val & bit::<Pos>()) >> Pos
+pub fn bit_of<const POS: u8>(val: u32) -> u32 {
+  (val & bit::<POS>()) >> POS
 }
 
-pub fn bit_of_range<const Msb: u8, const Lsb: u8>(val: u32) -> u32 {
-  if Msb < Lsb {
-    return bit_of_range::<Lsb, Msb>(val);
+pub fn bit_of_range<const MSB: u8, const LSB: u8>(val: u32) -> u32 {
+  if MSB < LSB {
+    return bit_of_range::<LSB, MSB>(val);
   }
-  (val & bit_range::<Msb, Lsb>()) >> Lsb
+  (val & bit_range::<MSB, LSB>()) >> LSB
 }
 
-pub fn bit_of_u64<const Pos: u8>(val: u64) -> u64 {
-  (val & bit_u64::<Pos>()) >> Pos
+pub fn bit_of_u64<const POS: u8>(val: u64) -> u64 {
+  (val & bit_u64::<POS>()) >> POS
 }
 
-pub fn bit_of_range_u64<const Msb: u8, const Lsb: u8>(val: u64) -> u64 {
-  if Msb < Lsb {
-    return bit_of_range_u64::<Lsb, Msb>(val);
+pub fn bit_of_range_u64<const MSB: u8, const LSB: u8>(val: u64) -> u64 {
+  if MSB < LSB {
+    return bit_of_range_u64::<LSB, MSB>(val);
   }
-  (val & bit_range_u64::<Msb, Lsb>()) >> Lsb
+  (val & bit_range_u64::<MSB, LSB>()) >> LSB
 }
